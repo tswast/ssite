f65ff9e948903a0ebf411bca5e7975055069cee6
Tim Swast <tswast@gmail.com>
2018-06-24 18:21:17 -0700

    Release 0.1.0.

--- setup.py
+++ setup.py
@@ -15,18 +15,22 @@
 import setuptools
 
 
+description = (
+    'Ssite is not a static site generator. '
+    'It is a collection of scripts to maintain a static site.'
+)
+version = '0.1.0'
+
+
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='ssite',
-    version='0.0.1',
+    version=version,
     author='Tim Swast',
     author_email='swast@google.com',
-    description=(
-        'Ssite is not a static site generator. '
-        'It is a collection of scripts to maintain a static site.'
-    ),
+    description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/tswast/ssite',
@@ -34,6 +38,7 @@ setuptools.setup(
         'beautifulsoup4>=4.4.1,<5.0dev',
         'html5lib>=0.9999999,<2.0dev',
         'Jinja2>=2.8,<3.0dev',
+        'setuptools>=28.0.0',
     ],
     entry_points={
         'console_scripts': [
